e6c82676bae1e3a297728983b80ea51d55058e30
mreyaz94 <mdreyaz94@gmail.com>
2021-11-03 18:47:23 +0530

    Allow

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
